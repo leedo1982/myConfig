561d2879bbe60e3c8e5fcaf4e61815dd24fdab76
domo18@gmail.com <domo18@gmail.com>
2018-02-18 08:44:00 +0900

    ready

---
+++ dotfiles/.hammerspoon2/README.md
@@ -0,0 +1,3 @@
+# hammerspoon
+mac 용 편의 도구 만들기.
+
---
+++ dotfiles/.hammerspoon2/init.lua
@@ -0,0 +1,13 @@
+-- hammerspoon config
+
+-- require('modules.caffein')
+-- require('modules.mouse')
+-- local caffein = require('modules.caffein'):init({'shift'}, 'f15')
+-- local vim_mode = require('modules.vim'):init('f13', 'f18')
+-- local inputChange = require('modules.inputSourceChange'):init({}, 'escape', 'com.apple.keylayout.ABC')
+-- local app_manager = require('modules.appman'):init('f15')
+-- local win_manager = require('modules.winman'):init('f16')
+-- local mouse = require('modules.mouse'):init('f14')
+
+local chage_lang = require('modules.mac_esc')
+local clipboard = require('modules.clipboard')
