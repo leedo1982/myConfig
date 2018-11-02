46c05b65cb92d13d74b8928d9715300c7d75f422
leedowon <domo18@gmail.com>
2018-11-02 17:56:12 +0900

    new keyboard

--- dotfiles/.hammerspoon/modules/mac_esc.lua
+++ dotfiles/.hammerspoon/modules/mac_esc.lua
@@ -2,17 +2,45 @@
 local normal_keyflag = false
 local control_keyflag = false
 
-
-
-control_keyevent = hs.eventtap.new({hs.eventtap.event.types.flagsChanged},function(event)
+-- 키 다운 이벤트 구독 
+normal_keydownevent = hs.eventtap.new({hs.eventtap.event.types.keyDown},function(event)
     local keycode = hs.keycodes.map[event:getKeyCode()]
 
-        print('rightCtrl !'+ keycode)
+    print("kedon event:", keycode)
 
-    if(keycode == 'escape') then 
-        print('rightCtrl & escape !!')
-        changeLanguage()
+    if(control_keyflag == true) then
+        normal_keyflag = true
+    elseif(control_keyflag == false) then
+        normal_keyflag = false
     end
+
+    if(keycode == "yen") then
+       -- hs.eventtap.keyStroke({}, )
+    end
+
+    
+end)
+normal_keydownevent:start()
+
+control_keyevent = hs.eventtap.new({hs.eventtap.event.types.flagsChanged},function(event)
+    local flags = event:getFlags()
+    local keycode = hs.keycodes.map[event:getKeyCode()]
+    print("kedon2 event:", keycode)
+
+    if(flags.ctrl == true) then
+        control_keyflag = true
+    elseif(flags.ctrl == nil) then
+        control_keyflag = false
+    end
+
+
+    if(keycode == 'ctrl' and flags.ctrl == nil and normal_keyflag == false) then 
+        print('Ctrl & escape !!')
+        changeLanguage()
+        hs.eventtap.keyStroke({}, 'escape')
+    end
+
+    normal_keyflag = false
 
     return false
 end)
