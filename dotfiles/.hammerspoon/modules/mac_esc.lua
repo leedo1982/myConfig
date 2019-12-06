47b7b11daf7015c5f7007b5d7d7224d35ec8ecf4
Leedo <leedo@deali.net>
2019-12-06 09:58:47 +0900

    ctrl+마우스 클릭시 escape 제거

--- dotfiles/.hammerspoon/modules/mac_esc.lua
+++ dotfiles/.hammerspoon/modules/mac_esc.lua
@@ -1,5 +1,6 @@
 
 local normal_keyflag = false
+local mouse_keyflag = false
 local control_keyflag = false
 
 -- 키 다운 이벤트 구독 
@@ -15,16 +16,34 @@ normal_keydownevent = hs.eventtap.new({hs.eventtap.event.types.keyDown},function
     end
 
     if(keycode == "yen") then
-       -- hs.eventtap.keyStroke({},'1' )
+    print("yen event:", keycode)
+       hs.eventtap.keyStroke({},'1' )
     end
 
     
 end)
 normal_keydownevent:start()
 
+-- 마우스 다운 이벤트 구독 
+normal_mousevent = hs.eventtap.new({hs.eventtap.event.types.leftMouseDown},function(event)
+    local mousecode = hs.keycodes.map[event:getKeyCode()]
+
+    print("mouse left down event:", mousecode)
+
+    if(control_keyflag == true) then
+        mouse_keyflag = true
+    elseif(control_keyflag == false) then
+       mouse_keyflag = false
+    end
+
+end)
+normal_mousevent:start()
+
 control_keyevent = hs.eventtap.new({hs.eventtap.event.types.flagsChanged},function(event)
     local flags = event:getFlags()
     local keycode = hs.keycodes.map[event:getKeyCode()]
+    print("kedon2 event:", keycode)
+    local mouse = hs.keycodes.map[event:getKeyCode()]
     print("kedon2 event:", keycode)
 
     if(flags.ctrl == true) then
@@ -34,13 +53,14 @@ control_keyevent = hs.eventtap.new({hs.eventtap.event.types.flagsChanged},functi
     end
 
 
-    if(keycode == 'ctrl' and flags.ctrl == nil and normal_keyflag == false) then 
+    if(keycode == 'ctrl' and flags.ctrl == nil and normal_keyflag == false and mouse_keyflag == false) then 
         print('Ctrl & escape !!')
         changeLanguage()
         hs.eventtap.keyStroke({}, 'escape')
     end
 
     normal_keyflag = false
+    mouse_keyflag = false
 
     return false
 end)
